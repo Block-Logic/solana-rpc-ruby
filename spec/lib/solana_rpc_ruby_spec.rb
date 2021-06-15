b533f69e9566563c4e49501fc8b401ed2e601e49
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2021-06-15 14:20:43 +0200

    Add specs for basic options

--- spec/lib/solana_rpc_ruby_spec.rb
+++ spec/lib/solana_rpc_ruby_spec.rb
@@ -1,2 +1,31 @@
 describe SolanaRpcRuby do
+  it 'is possible to provide config options' do
+    described_class.config do |c|
+      expect(c).to eq(described_class)
+    end
+  end
+
+  describe 'parameters' do
+    let(:fake_class) { class_double('SolanaRpcRuby') }
+    
+    it 'is possible to set account_pubkey' do
+      expect(fake_class).to receive(:account_pubkey=).with('account_pubkey')
+      fake_class.account_pubkey = 'account_pubkey'
+    end
+
+    it 'is possible to set json_rpc_url' do
+      expect(fake_class).to receive(:json_rpc_url=).with('json_rpc_url')
+      fake_class.json_rpc_url = 'json_rpc_url'
+    end
+
+    it 'is possible to set json_rpc_version' do
+      expect(fake_class).to receive(:json_rpc_version=).with('json_rpc_version')
+      fake_class.json_rpc_version = 'json_rpc_version'
+    end
+
+    it 'is possible to set identity' do
+      expect(fake_class).to receive(:identity=).with('identity')
+      fake_class.identity = 'identity'
+    end
+  end
 end
