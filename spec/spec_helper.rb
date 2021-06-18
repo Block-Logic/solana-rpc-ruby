663246a4b271db8d7b44089cd7b097df0a6f78b0
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2021-06-18 13:47:08 +0200

    Turn off codecov for now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,8 +9,8 @@ ENV['RAILS_ENV'] = 'test'
 ENV['RAILS_ROOT'] ||= "#{File.dirname(__FILE__)}../../../spec/dummy"
 
 if ENV['CI'] == 'true'
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+  # require 'codecov'
+  # SimpleCov.formatter = SimpleCov::Formatter::Codecov
 end
 
 SimpleCov.start 'rails' do
