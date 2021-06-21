c7819211e94793003ec089e4b4ac3395e819803c
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2021-06-21 17:09:26 +0200

    Add get_epoch_info, get_confirmed_blocks, get_vote_accounts methods withs specs

--- lib/solana_rpc_ruby/methods_wrapper.rb
+++ lib/solana_rpc_ruby/methods_wrapper.rb
@@ -25,6 +25,45 @@ module SolanaRpcRuby
       send_request(body, http_method)
     end
 
+    # https://docs.solana.com/developing/clients/jsonrpc-api#getepochinfo
+    def get_epoch_info(commitment: 'finalized')
+      http_method = :post
+      method =  create_method_name(__method__)
+      params = nil
+
+      body = create_json_body(method, method_params: params)
+
+      send_request(body, http_method)
+    end
+
+    # https://docs.solana.com/developing/clients/jsonrpc-api#getepochinfo
+    # DEPRECATED: Please use getBlocks instead This method is expected to be removed in solana-core v1.8
+    # Returns a list of confirmed blocks between two slots
+    def get_confirmed_blocks(start_slot:, end_slot: nil)
+      http_method = :post
+      method =  create_method_name(__method__)
+      params = [start_slot]
+      params << end_slot if end_slot # optional
+      body = create_json_body(method, method_params: params)
+
+      send_request(body, http_method)
+    end
+
+    # https://docs.solana.com/developing/clients/jsonrpc-api#getvoteaccounts
+    # Returns the account info and associated stake for all the voting accounts in the current bank.
+    def get_vote_accounts(commitment: nil, vote_pubkey: nil)
+      http_method = :post
+      method =  create_method_name(__method__)
+
+      params = []
+      params << commitment if commitment
+      params << vote_pubkey if vote_pubkey
+
+      body = create_json_body(method, method_params: params)
+
+      send_request(body, http_method)
+    end
+
     private
     def send_request(body, http_method)
       api_response = api_client.call_api(
@@ -44,7 +83,7 @@ module SolanaRpcRuby
     def create_method_name(method)
       return '' unless method
 
-      __method__.to_s.camelize(:lower)
+      method.to_s.camelize(:lower)
     end
   end
 end
--- spec/lib/solana_rpc_ruby/methods_wrapper_spec.rb
+++ spec/lib/solana_rpc_ruby/methods_wrapper_spec.rb
@@ -44,5 +44,57 @@ describe SolanaRpcRuby::MethodsWrapper do
         expect(response.id).to eq(1)
       end
     end
+
+    it '#get_epoch_info' do
+      VCR.use_cassette('get_epoch_info') do
+        response = described_class.new.get_epoch_info(commitment: 'finalized')
+        expected_result = {
+          "jsonrpc"=>"2.0", 
+          "result"=>{
+            "absoluteSlot"=>81852457, 
+            "blockHeight"=>67730579, 
+            "epoch"=>202, 
+            "slotIndex"=>112201, 
+            "slotsInEpoch"=>432000, 
+            "transactionCount"=>25644806326
+          }, 
+          "id"=>1
+        }
+
+        expect(response.result).to eq(
+          expected_result['result']
+        )
+        expect(response.json_rpc).to eq('2.0')
+        expect(response.id).to eq(1)
+      end
+    end
+
+    it '#get_confirmed_blocks' do
+      VCR.use_cassette('get_confirmed_blocks') do
+        response = described_class.new.get_confirmed_blocks(start_slot: 5, end_slot: 100)
+        expected_result = {
+          "jsonrpc"=>"2.0", 
+          "result"=>[], 
+          "id"=>1
+        }
+
+        expect(response.result).to eq(
+          expected_result['result']
+        )
+        expect(response.json_rpc).to eq('2.0')
+        expect(response.id).to eq(1)
+      end
+    end
+
+    it '#get_vote_accounts' do
+      VCR.use_cassette('get_vote_accounts') do
+        response = described_class.new.get_vote_accounts
+        
+        expect(response.result['current'].size).to eq(1914)
+        expect(response.result['delinquent'].size).to eq(205)
+        expect(response.json_rpc).to eq('2.0')
+        expect(response.id).to eq(1)
+      end
+    end
   end
 end
