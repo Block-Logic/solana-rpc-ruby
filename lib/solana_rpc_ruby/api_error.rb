3b865fdac268273f591e0f5ad12c2cb84bb812c9
Radoslaw Stolarski <radoslaw.stolarski@polcode.net>
2021-07-15 13:07:48 +0200

    Add to_s to message in ApiError

--- lib/solana_rpc_ruby/api_error.rb
+++ lib/solana_rpc_ruby/api_error.rb
@@ -6,20 +6,20 @@ module SolanaRpcRuby
     # Error code.
     # @return [Integer]
     attr_reader :code
-    
+
     # Error message.
     # @return [String]
     attr_reader :message
-    
+
     # Initialize object with json response from the API with error.
-    # 
+    #
     # @param code [Integer]
     # @param message [String]
-    # 
+    #
     # @return [SolanaRpcRuby::ApiError]
     def initialize(code: nil, message:)
       @code = code
-      @message = message
+      @message = message.to_s
 
       super message
     end
