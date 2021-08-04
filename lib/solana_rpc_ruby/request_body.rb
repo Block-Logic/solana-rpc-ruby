b1c3e75d168acc72465e8302fc288d82172a0cf8
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2021-08-04 14:04:01 +0200

    Add id to arguments passed into MethodsWrapper initializer + specs + readme update + docs update

--- lib/solana_rpc_ruby/request_body.rb
+++ lib/solana_rpc_ruby/request_body.rb
@@ -12,22 +12,27 @@ module SolanaRpcRuby
     # 
     # @param method [string] method name.
     # @param method_params [Array] ordered array with required and/or optional params.
+    # @param id [Integer] Unique client-generated identifying integer.
     # 
     # @return [Json] JSON string with body.
     # 
-    def create_json_body(method, method_params: [])
-      body = base_body
+    def create_json_body(method, method_params: [], id: @id)
+      body = base_body(id: id)
       body[:method] = method
       body[:params] = method_params if method_params.any?
       body.to_json
     end
 
     # Hash with default body params.
+    # @param id [Integer] Unique client-generated identifying integer.
+    # 
     # @return [Hash] hash with base params for every request.
-    def base_body
+    def base_body(id: 1)
+      raise ArgumentError, 'id must be an integer' unless id.is_a?(Integer)
+
       {
         "jsonrpc": SolanaRpcRuby.json_rpc_version,
-        "id": 1
+        "id": id
       }
     end
   end
--- spec/lib/solana_rpc_ruby/request_body_spec.rb
+++ spec/lib/solana_rpc_ruby/request_body_spec.rb
@@ -4,11 +4,29 @@ describe SolanaRpcRuby::RequestBody do
   it 'creates correct body from params' do
     method = 'getAccountInfo'
     params = [123, {'encoding': 'base58'}]
+    id = 99
 
-    json_body = create_json_body(method, method_params: params)
+    json_body = create_json_body(method, method_params: params, id: id)
 
-    expected_body = "{\"jsonrpc\":\"2.0\",\"id\":1,\"method\":\"getAccountInfo\",\"params\":[123,{\"encoding\":\"base58\"}]}"
+    expected_body = "{\"jsonrpc\":\"2.0\",\"id\":99,\"method\":\"getAccountInfo\",\"params\":[123,{\"encoding\":\"base58\"}]}"
     
     expect(json_body).to eq(expected_body)
+  end
+
+  it 'raises an error when id is not an integer' do
+    id = '1'
+    message = /id must be an integer/
+    expect { base_body(id: id) }.to raise_error(ArgumentError, message)
+  end
+
+  it 'returns correct base body' do
+    id = 5
+
+    expected_result = {
+      "jsonrpc": SolanaRpcRuby.json_rpc_version,
+      "id": id
+    }
+
+    expect(base_body(id: id)).to eq(expected_result)
   end
 end
